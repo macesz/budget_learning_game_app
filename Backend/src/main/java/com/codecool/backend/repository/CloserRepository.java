47e6a51b368e3d5eb1be0a0b07babdff963dca0c
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-05-19 19:04:19 +0200

    Acount controller created, services modifed where needed, tests modified

--- Backend/src/main/java/com/codecool/backend/repository/CloserRepository.java
+++ Backend/src/main/java/com/codecool/backend/repository/CloserRepository.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CloserRepository extends JpaRepository<Closer, Long> {
     List<Closer> findAllById(Long userId);
     Closer findFirstByHouseholdIdAndDateLessThanEqualOrderByDateDesc(Long householdId, LocalDate balanceDate);
     List<Closer> findByHouseholdIdOrderByDateDesc(Long householdId);
+    Closer findFirstByHouseholdIdOrderByDateDesc(Long householdId);
+    Optional<Closer> findByHouseholdIdAndDate(Long householdId, LocalDate date);
+
 
 }
