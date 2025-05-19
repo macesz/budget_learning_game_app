9a12a7d5ad5c402abf2da180c5e9e40f2b63315d
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-05-19 11:59:02 +0200

    HouseHoldService created

---
+++ Backend/src/main/java/com/codecool/backend/service/HouseHoldService.java
@@ -0,0 +1,28 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.model.entity.Household;
+import com.codecool.backend.repository.HouseholdRepository;
+import com.codecool.backend.repository.TransactionRepository;
+import com.codecool.backend.repository.UserEntityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HouseHoldService {
+
+    private final HouseholdRepository householdRepository;
+
+    public HouseHoldService(HouseholdRepository householdRepository) {
+
+        this.householdRepository = householdRepository;
+    }
+
+    public Optional<Household> findHouseholdById(Long id){
+        return householdRepository.findById(id);
+    }
+
+    public boolean deleteHouseholdById(Long id){
+        return householdRepository.deleteHouseholdById(id);
+    }
+}
