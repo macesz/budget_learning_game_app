a4b4be51d5fdf66b86723da62576832f3c781c2f
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-05-19 14:47:35 +0200

    fix household service

--- Backend/src/main/java/com/codecool/backend/repository/CloserRepository.java
+++ Backend/src/main/java/com/codecool/backend/repository/CloserRepository.java
@@ -3,10 +3,12 @@ package com.codecool.backend.repository;
 import com.codecool.backend.model.entity.Closer;
 import com.codecool.backend.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface CloserRepository extends JpaRepository<Closer, Long> {
     List<Closer> findAllById(Long userId);
     Closer getCloserByDateAndId(Long householdId, LocalDate balanceDate);
