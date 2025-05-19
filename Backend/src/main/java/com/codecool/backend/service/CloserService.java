f71b2ee8e8a1931b8cabf1a0d932d33bbef6c61f
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-05-19 11:08:50 +0200

    create closer service

--- Backend/src/main/java/com/codecool/backend/service/CloserService.java
+++ Backend/src/main/java/com/codecool/backend/service/CloserService.java
@@ -1,7 +1,19 @@
 package com.codecool.backend.service;
 
+import com.codecool.backend.model.entity.Closer;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
-public class CloserService {
+public class CloserService  implements ICloserService {
+    @Override
+    public Closer getLastCloser(Long householdId, LocalDate balanceDate) {
+        return null;
+    }
+
+    @Override
+    public Closer createCloser(Long householdId, LocalDate closerDate) {
+        return null;
+    }
 }
